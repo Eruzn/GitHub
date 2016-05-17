54c4239364e2064e9bcc687e37edc9b2ee68ee33
Eruzn-Hui <huiyiwangji@hotmail.com>
2016-05-17 21:47:34 +0800

    fix

--- Hello_World/taxifee.c
+++ Hello_World/taxifee.c
@@ -4,23 +4,12 @@ float taxifee(int time, int miles)
 {
 	float money;
 	if (miles <= 3)
-	{
 		money = 13 + 1;
-		printf("起步车费为%f元\n", money);
-	}
-	else
-	{
+  else
 		if (time >= 23 || time < 5)
-		{
 			money = 13 + 1 + (miles - 3)*2.3*1.2;
-			printf("夜间车费为%f元\n", money);
-		}
 		else
-		{
 			money = 13 + 1 + (miles - 3)*2.3;
-			printf("日间车费为%f元\n", money);
-		}
-	}
 	return money;
 }
 
