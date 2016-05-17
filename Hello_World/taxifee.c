77aa3a94faefe20973de05f8db582c3e49520d56
Eruzn-Hui <huiyiwangji@hotmail.com>
2016-05-17 21:49:32 +0800

    fix

--- Hello_World/taxifee.c
+++ Hello_World/taxifee.c
@@ -6,10 +6,12 @@ float taxifee(int time, int miles)
 	if (miles <= 3)
 		money = 13 + 1;
   else
+	{
 		if (time >= 23 || time < 5)
 			money = 13 + 1 + (miles - 3)*2.3*1.2;
 		else
 			money = 13 + 1 + (miles - 3)*2.3;
+	}
 	return money;
 }
 
